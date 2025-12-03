f8a954a5e8881b028b0b958899b7a8ff330e6ce4
Sagar <Sagar.arete@gmail.com>
2025-12-03 17:51:08 +0530

    user.models.js in this file add some methods

--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 const userSchema = new mongoose.Schema({
@@ -7,3 +9,20 @@ const userSchema = new mongoose.Schema({
     password: {type: String,required : true,select: false},
     socketId: {type: String}
 })
+
+// for generate jwt token
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET);
+    return token;
+}
+
+// for compare passwords
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+// for genrate hashed password 
+userSchema.statics.hashPassword = async function(password){
+    return await bcrypt.hash(password, 10);
+}
+
